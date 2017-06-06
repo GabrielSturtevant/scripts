6c2e7fc1340e7ed9cbf18eeca8c8adc9ff39ac91
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-06 15:24:38 -0700

    Test tty input

--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -1,10 +1,15 @@
-import os, shutil
+import os, shutil, serial
+
+usr_input = serial.Serial('/dev/tty')
+print('enter something: ')
+something = usr_input.read()
+print(something)
 
 print('Running Ubuntu Homestead installation script')
 #  Checks whether the user has configured an ssh key
 if not os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
     print('ssh key has not been configured.')
-    email = input('Please enter your email address:')
+    # email = input('Please enter your email address:')
 
 # Ensures dependencies are met
 print('Checking whether Git is installed')
