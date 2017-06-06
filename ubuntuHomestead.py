78d32fd130f9dd7fc0110ab285b7a5532e7819e4
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-06 14:20:10 -0700

    Raw isn't working right

--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -1,6 +1,10 @@
 import os
 
 print('Running Ubuntu Homestead installation script')
+#  Checks whether the user has configured an ssh key
 if not os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
     print('ssh key has not been configured.')
     email = input('Please enter your email address:')
+    print('Sweeet')
+
+#
