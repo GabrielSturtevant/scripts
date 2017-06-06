2fa666f5a98f3d80693237227750846529e87a4e
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-06 15:08:47 -0700

    Install portion started

--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -1,10 +1,15 @@
-import os
+import os, shutil
 
 print('Running Ubuntu Homestead installation script')
 #  Checks whether the user has configured an ssh key
 if not os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
     print('ssh key has not been configured.')
     email = input('Please enter your email address:')
-    print('Sweeet')
 
-#
+# Ensures dependencies are met
+print('Checking whether Git is installed')
+if shutil.which('git') is None:
+    print('Git is not installed, installing now')
+    os.system('sudo apt install git -y')
+else:
+    print('Git is already installed')
