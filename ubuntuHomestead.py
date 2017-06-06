a79aa29a925e9276b23862e41dc2669103168170
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-06 14:14:34 -0700

    Added email input

--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -2,7 +2,6 @@
 import os
 
 print('Running Ubuntu Homestead installation script')
-if os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
-    print('true')
-else:
-    print('false')
+if not os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
+    print('ssh key has not been configured.')
+    email = input('Please enter your email address:')
