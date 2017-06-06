8f0e7650600b047f7e49f5ddb66950d53dbff5d7
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-06 14:03:44 -0700

    Testing

--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3.5
+import os
+
+print('Running Ubuntu Homestead installation script')
+if os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
+    print('true')
+else:
+    print('false')
