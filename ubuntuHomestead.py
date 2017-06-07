0b1f6040ce9758ff1a34663a53c2de8550fd5379
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-06 21:34:33 -0700

    Almost done

--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -4,6 +4,7 @@ import shutil
 import time
 import requests
 import hashlib
+import getopt
 
 
 ## Program constants
@@ -12,110 +13,131 @@ STATIC_IP = "192.168.10.10"
 URL_NAME = "homestead.app"
 INITIAL_PATH = os.getcwd()
 HOST_PERMISSIONS = "644"
-FRAMEWORK_PATH = "/First/second/third/fourth"
+FRAMEWORK_PATH = "Code"
+INSTALLED_SSH = False
+DEFAULT_FRAMEWORK_URL = "https://github.com/laravel/laravel.git"
+DEFAULT_FW_DIR_NAME = 'Laravel'
+DEFAULT_APP_NAME = 'homestead'
+NUMBER_OF_CPUS = '1'
 
 ## System modifications
 sys.stdin = open('/dev/tty')
 
-#
-# def md5(fname):
-#     hash_md5 = hashlib.md5()
-#     with open(fname, "rb") as f:
-#         for chunk in iter(lambda: f.read(4096), b""):
-#             hash_md5.update(chunk)
-#     return hash_md5.hexdigest()
-#
-#
-# def cs():
-#     os.system('clear')
-#
-#
-# def install(program_name, common_name):
-#     print('Checking whether {} is installed'.format(common_name))
-#     if shutil.which(program_name) is None:
-#         print('{} is not installed, installing now'.format(common_name))
-#         os.system('sudo apt install {} -y'.format(program_name))
-#     else:
-#         print('{} is already installed'.format(common_name))
-#
-#     time.sleep(.5)
-#     cs()
-#
-#
-# cs()
-# print('Running Ubuntu Homestead installation script')
-# #  Checks whether the user has configured an ssh key
-# if not os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
-#     print('ssh key has not been configured.')
-#     email = input('Please enter your email address:')
-#     os.system('ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -C "{}" -N ""'.format(email))
-#
-# time.sleep(1)
-# cs()
-#
-# # Update system
-# print('Updating system')
-# os.system('sudo apt update')
-#
-# time.sleep(1)
-# cs()
-#
-# # Ensures dependencies are met
-# install('git', 'Git')
-# install('virtualbox', 'VirtualBox')
-# install('vim', 'Vim')
-# install('python-pip', 'Pip')
-#
-# # Install python dependencies
-# os.system('sudo pip install beautifulsoup4')
-# os.system('sudo pip install requests')
-# os.system('sudo pip install lxml')
-#
-# os.system('rm -rf vagrant*.deb')
-#
-# time.sleep(1)
-# file_name = 'temp.py'
-# python_program = open(file_name, 'w+')
-# r = requests.get('https://raw.githubusercontent.com/GabrielSturtevant/scripts/master/GetVagrantLink.py')
-# python_program.write(r.content.decode('utf-8'))
-# python_program.close()
-#
-# if md5(file_name) == FILE_HASH:
-#     os.system('wget $(python {})'.format(file_name))
-# else:
-#     print('Python script integrity compromised. Exiting now')
-#     exit(1)
-#
-# os.system('rm -f {}'.format(file_name))
-#
-# os.system('sudo dpkg -i vagrant*.deb')
-#
-# os.system('rm -rf vagrant*.deb')
 
-# print('Attempting to edit /etc/hosts, a backup will be created at /etc/hosts.BAK')
-# new_hosts = open('hosts', 'w+')
-# old_hosts = open('/etc/hosts', 'r')
-# new_hosts.write(old_hosts.read())
-#
-# old_hosts.close()
-# os.system('sudo cp /etc/hosts /etc/hosts.BAK')
-#
-# to_write = "\n# Homestead ip address and url\n"
-# to_write += "{}\t{}".format(STATIC_IP, URL_NAME)
-# new_hosts.write(to_write)
-# new_hosts.close()
-#
-# os.system('sudo rm /etc/hosts')
-# os.system('mv hosts /etc/hosts')
-# os.system('sudo chmod {} /etc/hosts'.format(HOST_PERMISSIONS))
-# print('Finished editing hosts file')
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def cs():
+    os.system('clear')
+
+
+def install(program_name, common_name):
+    print('Checking whether {} is installed'.format(common_name))
+    if shutil.which(program_name) is None:
+        print('{} is not installed, installing now'.format(common_name))
+        os.system('sudo apt install {} -y'.format(program_name))
+    else:
+        print('{} is already installed'.format(common_name))
+
+    time.sleep(.5)
+    cs()
+
+help_out = 'This provides a small list of command line arguments that this program will accept:' \
+           '\t-u\tEnter a custom url to fetch a Laravel repository from\n' \
+           '\t-n\tEnter a custom application name\n' \
+           '\t-l\tEnter a custom link name ex: homestead = homestead.app'
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"uh")
+except getopt.GetoptError:
+    print('names.py -f -l -e -d -n <number of names to print>')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print(help_out)
+        sys.exit(1)
+
+cs()
+print('Running Ubuntu Homestead installation script')
+#  Checks whether the user has configured an ssh key
+if not os.path.isfile(os.path.expanduser('~') + '/.ssh/id_rsa.pub'):
+    INSTALLED_SSH = True
+    print('ssh key has not been configured.')
+    email = input('Please enter your email address:')
+    os.system('ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -C "{}" -N ""'.format(email))
+
+time.sleep(1)
+cs()
+
+# Update system
+print('Updating system')
+os.system('sudo apt update')
+
+time.sleep(1)
+cs()
+
+# Ensures dependencies are met
+install('git', 'Git')
+install('virtualbox', 'VirtualBox')
+install('vim', 'Vim')
+install('python-pip', 'Pip')
+
+# Install python dependencies
+os.system('sudo pip install beautifulsoup4')
+os.system('sudo pip install requests')
+os.system('sudo pip install lxml')
+
+os.system('rm -rf vagrant*.deb')
+
+time.sleep(1)
+file_name = 'temp.py'
+python_program = open(file_name, 'w+')
+r = requests.get('https://raw.githubusercontent.com/GabrielSturtevant/scripts/master/GetVagrantLink.py')
+python_program.write(r.content.decode('utf-8'))
+python_program.close()
+
+if md5(file_name) == FILE_HASH:
+    os.system('wget $(python {})'.format(file_name))
+else:
+    print('Python script integrity compromised. Exiting now')
+    exit(1)
+
+os.system('rm -f {}'.format(file_name))
+
+os.system('sudo dpkg -i vagrant*.deb')
+
+os.system('rm -rf vagrant*.deb')
+
+print('Attempting to edit /etc/hosts, a backup will be created at /etc/hosts.BAK')
+new_hosts = open('hosts', 'w+')
+old_hosts = open('/etc/hosts', 'r')
+new_hosts.write(old_hosts.read())
+
+old_hosts.close()
+os.system('sudo cp /etc/hosts /etc/hosts.BAK')
+
+to_write = "\n# Homestead ip address and url\n"
+to_write += "{}\t{}".format(STATIC_IP, URL_NAME)
+new_hosts.write(to_write)
+new_hosts.close()
+
+os.system('sudo rm /etc/hosts')
+os.system('mv hosts /etc/hosts')
+os.system('sudo chmod {} /etc/hosts'.format(HOST_PERMISSIONS))
+print('Finished editing hosts file')
 
 # Go to home directory
 os.chdir(os.path.expanduser('~'))
 
-# os.system('vagrant box add laravel/homestead --provider virtualbox')
-#
-# os.system('git clone https://github.com/laravel/homestead.git Homestead')
+os.system('vagrant box add laravel/homestead --provider virtualbox')
+
+os.system('git clone https://github.com/laravel/homestead.git Homestead')
 
 path = FRAMEWORK_PATH
 
@@ -123,8 +145,10 @@ while True:
     path = path.split('/')
     if path[0] == '':
         path.pop()
+
     try:
         os.makedirs(os.path.join(os.path.expanduser('~'), *path))
+        FRAMEWORK_PATH = '/'.join(path)
         break
     except FileExistsError:
         print('Oops, looks like that directory already exists\n')
@@ -132,3 +156,26 @@ while True:
                      ' in \n(type N to place it in existing directory):\n')
         if path.lower() == 'n':
             break
+
+os.system('cd {}'.format(FRAMEWORK_PATH))
+os.system('git clone {} {}'.format(DEFAULT_FRAMEWORK_URL, DEFAULT_FW_DIR_NAME))
+
+os.chdir(os.path.expanduser('~') + '/Homestead')
+os.system('chmod +x init.sh; ./init.sh')
+
+homestead_yaml = open('Homestead.yaml', 'r+')
+new_yaml = open('Homestead.yaml.new', 'r+')
+info = homestead_yaml.read()
+
+for line in info:
+    if 'Code/Laravel/Public' in line:
+        line = line.replace('Code/Laravel', '{}/{}'.format(FRAMEWORK_PATH, DEFAULT_APP_NAME))
+
+    if 'cpus: 1' in line:
+        line = line.replace('1', NUMBER_OF_CPUS)
+
+    if 'homestead.app' in line:
+        line = line.replace('homestead.app', URL_NAME)
+    new_yaml.write(line)
+os.system('rm Homestead.yaml; mv Homestead.yaml.new Homestead.yaml')
+
