78dc37e5f59845c0ec5d0050048b3dda6d6c6c1e
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-06-12 10:17:17 -0700

    Fixed toolbox install script

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 # This script installs the jetbrains toolbox application
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
-    exit
-fi
 if ! type jq > /dev/null; then
     apt install jq -y
 fi
-cd ~/Downloads
+pushd $HOME/Downloads
 rm -f jetbrains-toolbox*.tar.gz
 wget $(curl https://data.services.jetbrains.com/products/releases?code=TBA | jq -r .TBA[0].downloads.linux.link)
-rm -rf /opt/jetbrains*
-mv jetbrains-toolbox* /opt/
-cd /opt/
-mv jetbrains-toolbox*.tar.gz jetbrains-toolbox.tar.gz
-tar -zxf jetbrains-toolbox.tar.gz
-rm -rf jetbrains*.tar.gz
-mv jetbrains-toolbox-* jetbrains-toolbox
-sudo -k
+sudo rm -rf /opt/jetbrains*
+sudo mv jetbrains-toolbox* /opt/
+pushd /opt/
+sudo mv jetbrains-toolbox*.tar.gz jetbrains-toolbox.tar.gz
+sudo tar -zxf jetbrains-toolbox.tar.gz
+sudo rm -rf jetbrains*.tar.gz
+sudo mv jetbrains-toolbox-* jetbrains-toolbox
 ./jetbrains-toolbox/jetbrains-toolbox
